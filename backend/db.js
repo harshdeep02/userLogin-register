e26ace5caf5686745ba8dbd99fe3005de86d20f0
Harsh Deep <harshdeep7887@gmail.com>
2024-11-27 19:17:19 +0530

    first commit2

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToMongo = async()=>{
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/userData')
+        await mongoose.connect('mongodb+srv://harshdeep7887:harshdeepauth@login-registration2.ki6bb.mongodb.net/?retryWrites=true&w=majority&appName=login-registration2')
         .then(()=>{
             console.log("connected sucessfully");
         })
