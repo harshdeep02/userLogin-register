2243af333079cd66b49e204c2755b68fa6c118f1
Harsh Deep <137142567+harshdeep02@users.noreply.github.com>
2024-11-27 19:27:20 +0530

    Update RegisterUser.jsx with backend url

--- frontend/src/Components/RegisterUser.jsx
+++ frontend/src/Components/RegisterUser.jsx
@@ -22,7 +22,7 @@ export const RegisterUser = () => {
      setLoading(true)
 
      try{
-     const response = await fetch("http://127.0.0.1:5000/register", {
+     const response = await fetch("https://userlogin-register-backend.onrender.com/register", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
